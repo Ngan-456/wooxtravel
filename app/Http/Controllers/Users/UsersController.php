2e14dd37241aafa82eb5d03b17e4630255c55f9f
Ngân <thungandn04@gmail.com>
2024-12-17 23:24:46 +0700

    first commit

---
+++ app/Http/Controllers/Users/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Users;
+
+use App\Http\Controllers\Controller;
+use App\Models\Reservation\Reservation;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function bookings(){
+        $bookings = Reservation::where('user_id', Auth::user()->id)
+            ->orderBy('id', 'desc')
+            ->get();
+
+        return view('users.bookings', compact('bookings'));
+    }
+}
